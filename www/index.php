88b089da92cceb0ff4ea7a244b37698735b6bd8a
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2017-12-02 21:46:40 +0200

    Make it easy to deploy with docker-compose

---
+++ www/index.php
@@ -0,0 +1,9 @@
+<?php
+$db_host = "mysql";
+$db_user = "root";
+$db_password = "root";
+$db_name = "odk";
+
+$db_conn = mysqli_connect($db_host, $db_user, $db_password, $sb_name) or die(mysqli_connect_error());
+echo "OK, let us do it"
+?>
